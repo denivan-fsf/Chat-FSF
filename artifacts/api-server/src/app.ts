87965789eea58bd4967d8d073aef99f32b732624
denivan-fsf <denivan@fazsaofrancisco.com>
2026-09-16 08:13:53 -0300

    feat(messages): aumentar limite para envio de arquivos

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -36,8 +36,8 @@ app.use(cors({
   credentials: true,
 }));
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
 app.use("/api", router);
 
