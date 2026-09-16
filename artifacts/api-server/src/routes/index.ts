2ac73f49f95780237f7f36bf8a54fada2fbfcf5b
denivan-fsf <denivan@fazsaofrancisco.com>
2026-09-16 14:40:39 -0300

    fix: restore proven outbound media sender

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -4,7 +4,6 @@ import mediaWebhookRouter from "./media-webhook";
 import mediaUrlRouter from "./media-url";
 import mediaImageFixRouter from "./media-image-fix";
 import mediaDownloadRouter from "./media-download";
-import imageSendFixRouter from "./image-send-fix";
 import advancedMessagesRouter from "./advanced-messages";
 import sharedInboxRouter from "./shared-inbox";
 
@@ -15,7 +14,6 @@ router.use(mediaWebhookRouter);
 router.use(mediaUrlRouter);
 router.use(mediaImageFixRouter);
 router.use(mediaDownloadRouter);
-router.use(imageSendFixRouter);
 router.use(advancedMessagesRouter);
 router.use(sharedInboxRouter);
 
